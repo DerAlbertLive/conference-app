a35fdf19502a6fa019fa49e9f1d29f0967707c07
Albert Weinert <info@der-albert.com>
2019-01-20 12:30:10 +0100

    after lot of hick hack, we have a new manifest.json and generade app icons

---
+++ vue.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
+module.exports = {
+    pwa: {
+        name: "Conference App",
+        themeColor: '#2196f3',
+        msTileColor: '#2196f3',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        iconPaths:{
+        favicon32: 'icons/android/icon_32x32.png',
+        favicon16: 'icons/android/icon_32x32.png',
+        appleTouchIcon: 'icons/ios/icon_152x152.png',
+        maskIcon: '',
+        msTileImage: 'icons/android/icon_144x144.png',
+        },
+    },
+    configureWebpack: {
+        plugins: [
+            // new WebpackPwaManifest({
+            //     name: 'Conference App',
+            //     short_name: 'vcapp',
+            //     description: 'Der Session Planer für die Konferenz der Wahl',
+            //     background_color: '#2196f3',
+            //     theme_color: '#2196f3',
+            //     inject: true,
+            //     fingerprints: false,
+            //     ios: {
+            //         'apple-mobile-web-app-title': 'Conference Sessions',
+            //         'apple-mobile-web-app-status-bar-style': 'black-translucent'
+            //     },
+            //     icons: [
+            //         {
+            //             src: path.resolve('src/assets/logo.png'),
+            //             sizes: [120, 152, 167, 180, 1024],
+            //             destination: path.join('icons', 'ios'),
+            //             ios: true
+            //         },
+            //         {
+            //             src: path.resolve('src/assets/logo.png'),
+            //             size: 1024,
+            //             destination: path.join('icons', 'ios'),
+            //             ios: 'startup'
+            //           },
+            //           {
+            //             src: path.resolve('src/assets/logo.png'),
+            //             sizes: [32, 48, 36, 48, 72, 96, 144, 192, 512],
+            //             destination: path.join('icons', 'android')
+            //           }
+            //     ]
+            // }),
+        ]
+    }    
+}
